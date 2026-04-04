9a570764e6992e0d591111850f2243d7915b0171
Akash Mishra <akashmishra967033@gmail.com>
2026-04-04 17:57:18 +0530

    Fixed Meeting import and history API

--- Backend/src/routes/UserRoutes.js
+++ Backend/src/routes/UserRoutes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { getUserHistory, login, register, addToHistory } from "../controller/UserController.js";
+import { addToHistory, getUserHistory, login, register } from "../controllers/user.controller.js";
 
 
 
 const router = Router();
-router.route("/login").post(login);
-router.route("/register").post(register);
+
+router.route("/login").post(login)
+router.route("/register").post(register)
 router.route("/add_to_activity").post(addToHistory)
 router.route("/get_all_activity").get(getUserHistory)
 
