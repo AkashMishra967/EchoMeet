2d3aa3d09b8535564406d20a6d3c1b6e4b343197
Akash Mishra <akashmishra967033@gmail.com>
2026-04-04 17:22:06 +0530

    Fixed frontend tracking issue

---
+++ frontend/src/App.js
@@ -0,0 +1,31 @@
+
+import './App.css';
+import {Route,BrowserRouter as Router,Routes} from 'react-router-dom';
+import LandingPage from './pages/landing';
+import Authentication from './pages/Authentication.js'
+import { AuthProvider } from './context/AuthContext.js';
+import HomeComponent from './pages/home';
+import VideoMeetComponent from './pages/VideoMeet.js';
+import History from './pages/history.js';
+
+function App() {
+  return(
+    <>
+<Router>
+  <AuthProvider>
+  <Routes>
+    <Route path='/'element={<LandingPage/>} />
+    <Route path="auth" element={<Authentication />} />
+    <Route path="/history" element={<History />} />
+  <Route path="/:url" element={<VideoMeetComponent />} />
+  <Router path='/:home' element={<HomeComponent />} />
+  </Routes>
+  </AuthProvider>
+</Router>
+
+
+    </>
+  );
+}
+
+export default App;
